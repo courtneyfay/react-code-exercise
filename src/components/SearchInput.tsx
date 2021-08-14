f7581efe323ff2205a1d4bdb3ce43266f242ba92
Courtney Fay <courtneyfay@users.noreply.github.com>
2021-08-14 10:49:38 -0600

    Adding functionality to button click

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styled from 'styled-components';
 import Label from '../styled-components/Label';
 import Input from '../styled-components/Input';
 import { PrimaryButton } from '../styled-components/Button';
 
-const Wrapper = styled.div`
+const FormWrapper = styled.form`
     width: 45%;
     display: flex;
     flex-direction: row;
@@ -14,12 +14,39 @@ const Wrapper = styled.div`
 `;
 
 const SearchInput = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        console.log('hitting handleSubmit', searchTerm)
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value)
+    }
+
     return (
-        <Wrapper>
-            <Label htmlFor="search-param">Choose a search term:</Label>
-            <Input type="text" id="search-param" name="search-param" />
-            <PrimaryButton type="submit">Search</PrimaryButton>
-        </Wrapper>
+        <FormWrapper
+            onSubmit={handleSubmit}
+        >
+            <Label htmlFor="search-param">
+                Choose a search term: 
+            </Label>
+            <Input
+                type="text"
+                id="search-param"
+                name="search-param"
+                autoComplete="off"
+                value={searchTerm}
+                onChange={handleChange}
+            />
+            <PrimaryButton
+                type="submit"
+                value="Submit"
+            >
+                Search
+            </PrimaryButton>
+        </FormWrapper>
     )
 };
 
